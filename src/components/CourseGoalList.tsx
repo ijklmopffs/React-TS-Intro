7bf363421f85e67e5a8935c1caa96db227f10ec3
Nuel Elekwachi <elekwaeke300@gmail.com>
2024-05-14 21:46:42 +0100

    first commit

---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,26 @@
+import CourseGoal from "./CourseGoal";
+import { CourseGoal as Course } from "../App";
+
+type CourseGoalListProps = {
+  goals: Course[];
+  onDeleteGoal: (id: number) => void;
+};
+
+export default function CourseGoalList({
+  goals,
+  onDeleteGoal,
+}: CourseGoalListProps) {
+  return (
+    <>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
