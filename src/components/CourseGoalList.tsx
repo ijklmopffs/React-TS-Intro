9e6a043bb106813779edffe314be804ba8ec22e7
Nuel Elekwachi <elekwaeke300@gmail.com>
2024-05-18 23:24:51 +0100

    added infobox and learnt about discriminated union

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react";
 import CourseGoal from "./CourseGoal";
 import { CourseGoal as Course } from "../App";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: Course[];
@@ -10,8 +12,27 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
     <>
+      {warningBox}
       <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
